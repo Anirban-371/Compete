4bf5c9487076864dbb68663d6301902d6c602b6b
Anirban <89.anirbandey@gmail.com>
2022-06-17 11:47:53 +0530

    modified pojo class

--- src/main/java/com/edifyine/competition/repository/QuestionAnswerRepository.java
+++ src/main/java/com/edifyine/competition/repository/QuestionAnswerRepository.java
@@ -3,9 +3,9 @@ package com.edifyine.competition.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.edifyine.competition.pojo.QuestionAnswer;
+import com.edifyine.competition.pojo.Topic;
 
-public interface QuestionAnswerRepository extends MongoRepository<QuestionAnswer, Long> {
-
+public interface QuestionAnswerRepository extends MongoRepository<Topic, Long> {
 	void findByTopicCode(String topicCode);
 
 }
